148768c3cdd3ca57226d9bab91957ea02cff6f7d
Caio Monteiro <caiomonteiropro@gmail.com>
2026-01-06 10:40:23 -0300

    adicionando models e enum

---
+++ src/Service/Users/Users.API/Enums/EnumProfile.cs
@@ -0,0 +1,8 @@
+﻿namespace Users.API.Enums;
+
+public enum EnumProfile
+{
+    // Membros do enum, separados por vírgula
+        Administrador = 1,
+        Usuario = 2
+}
---
+++ src/Service/Users/Users.API/Models/UserModel.cs
@@ -0,0 +1,27 @@
+﻿using Users.API.Enums;
+
+namespace Users.API.Models; 
+
+public class UserModel
+{
+    public int IdUser { get; set; }
+    public string Name { get; set; }
+    public string Login { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+    public DateTime DateBirth { get; set; }
+    public EnumProfile IdProfile { get; set; }
+
+    public UserModel() { }
+
+    public UserModel(int idUser, string name, string login, string password, string email, DateTime dateBirth, EnumProfile idProfile)
+    {
+        IdUser = idUser;
+        Name = name;
+        Login = login;
+        Password = password;
+        Email = email;
+        DateBirth = dateBirth;
+        IdProfile = idProfile;
+    }
+}
