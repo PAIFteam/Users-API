4c8066bdcef54bb9d570f3a45d0da3d539f82e96
Caio Monteiro <caiomonteiropro@gmail.com>
2026-01-06 10:55:52 -0300

    adicionado arquitetura vertical

---
+++ src/Service/Users/Users.API/Users/CreateUser/CreateUserEndpoint.cs
@@ -0,0 +1,6 @@
+﻿namespace Users.API.Users.CreateUser;
+
+//will be our endpoint for creating a user
+public class CreateUserEndpoint
+{
+}
---
+++ src/Service/Users/Users.API/Users/CreateUser/CreateUserHandler.cs
@@ -0,0 +1,6 @@
+﻿namespace Users.API.Users.CreateUser;
+
+//will be our logical handler for creating a user
+public class CreateUserHandler
+{
+}
