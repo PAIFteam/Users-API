72df54baf167db490f98f23dc6de9f16afdb5b75
Caio Monteiro <caiomonteiropro@gmail.com>
2026-01-06 11:03:14 -0300

    adicionando primeiras lógicas à criação de users

--- src/Service/Users/Users.API/Users/CreateUser/CreateUserHandler.cs
+++ src/Service/Users/Users.API/Users/CreateUser/CreateUserHandler.cs
@@ -1,6 +1,17 @@
-﻿namespace Users.API.Users.CreateUser;
+﻿using MediatR;
+using Users.API.Enums;
+
+namespace Users.API.Users.CreateUser;
 
 //will be our logical handler for creating a user
-public class CreateUserHandler
+
+public record CreateUserCommand(string Name, string Login, string Password, string Email, DateTime DateBirth, EnumProfile IdProfile)
+    : IRequest<CreateUserResult>;
+public record CreateUserResult(int IdUser);
+internal class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, CreateUserResult>
 {
+    public Task<CreateUserResult> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
 }
