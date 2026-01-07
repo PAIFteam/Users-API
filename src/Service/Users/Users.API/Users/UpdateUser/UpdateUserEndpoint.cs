4ed76cb3a4ecab44c7089bb11cc84de8fdf95280
Caio Monteiro <caiomonteiropro@gmail.com>
2026-01-07 15:50:26 -0300

    adicionando update user

---
+++ src/Service/Users/Users.API/Users/UpdateUser/UpdateUserEndpoint.cs
@@ -0,0 +1,5 @@
+﻿namespace Users.API.Users.UpdateUser; 
+
+public class UpdateUserEndpoint
+{
+}
---
+++ src/Service/Users/Users.API/Users/UpdateUser/UpdateUserHandler.cs
@@ -0,0 +1,34 @@
+﻿namespace Users.API.Users.UpdateUser; 
+
+public record UpdateUserCommand(Guid IdUser, string Name, string Login, string Password,
+    string Email, DateTime DateBirth, EnumProfile IdProfile) : ICommand<UpdateUserResult>;
+
+public record UpdateUserResult(bool IsSuccess);
+
+internal class UpdateUserCommandHandler(IDocumentSession session, ILogger<UpdateUserCommandHandler> logger)
+    : ICommandHandler<UpdateUserCommand, UpdateUserResult>
+{
+    public async Task<UpdateUserResult> Handle(UpdateUserCommand command, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("UpdateUserCommandHandler.Handle called with {@Command}", command);
+
+        var users = await session.LoadAsync<UserModel>(command.IdUser, cancellationToken);
+
+        if(users is null)
+        {
+            throw new UserNotFoundException(); 
+        }
+
+        users.Name = command.Name;
+        users.Login = command.Login;
+        users.Password = command.Password;
+        users.Email = command.Email;
+        users.DateBirth = command.DateBirth;
+        users.IdProfile = command.IdProfile;
+
+        session.Update(users);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new UpdateUserResult(true); 
+    }
+}
