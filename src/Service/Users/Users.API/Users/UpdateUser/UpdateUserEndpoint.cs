0871980a1c2cf8ad82a415463f24abdedc6f6a5b
Caio Monteiro <caiomonteiropro@gmail.com>
2026-01-07 15:58:28 -0300

    finalizando update user

--- src/Service/Users/Users.API/Users/UpdateUser/UpdateUserEndpoint.cs
+++ src/Service/Users/Users.API/Users/UpdateUser/UpdateUserEndpoint.cs
@@ -1,5 +1,26 @@
-﻿namespace Users.API.Users.UpdateUser; 
+﻿
+namespace Users.API.Users.UpdateUser; 
 
-public class UpdateUserEndpoint
+public record UpdateUserRequest(Guid IdUser, string Name, string Login, string Password,
+    string Email, DateTime DateBirth, EnumProfile IdProfile);
+
+public record UpdateUserResponse(bool IsSuccess);
+public class UpdateUserEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/users", async (ISender sender, UpdateUserRequest request, CancellationToken cancellationToken) =>
+        {
+            var command = request.Adapt<UpdateUserCommand>();
+            var result = await sender.Send(command, cancellationToken);
+            var response = result.Adapt<UpdateUserResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("UpdateUser")
+        .Produces<UpdateUserResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Update User")
+        .WithDescription("Update User");
+    }
 }
