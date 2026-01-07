33e53d5c5b68c06d7a55bd1857276f600eb9cbf6
Caio Monteiro <caiomonteiropro@gmail.com>
2026-01-07 17:30:29 -0300

    finalizando user delete

---
+++ src/Service/Users/Users.API/Users/DeleteUser/DeleteUserEndpoint.cs
@@ -0,0 +1,21 @@
+﻿namespace Users.API.Users.DeleteUser;
+
+//public record DeleteUserRequest(Guid IdUser);
+public record DeleteUserResponse(bool IsSuccess);
+public class DeleteUserEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/users/{idUser:guid}", async (ISender sender, Guid idUser, CancellationToken cancellationToken) =>
+        {
+            var result = await sender.Send(new DeleteUserCommand(idUser));
+            var response = result.Adapt<DeleteUserResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("DeleteUser")
+        .Produces<DeleteUserResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Delete User")
+        .WithDescription("Delete User");
+    }
+}
---
+++ src/Service/Users/Users.API/Users/DeleteUser/DeleteUserHandler.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Users.API.Users.DeleteUser; 
+public record DeleteUserCommand(Guid IdUser) : ICommand<DeleteUserResult>;
+public record DeleteUserResult(bool IsSuccess);
+internal class DeleteUserCommandHandler(IDocumentSession session, ILogger<DeleteUserCommandHandler> logger)
+    : ICommandHandler<DeleteUserCommand, DeleteUserResult>
+{
+    public async Task<DeleteUserResult> Handle(DeleteUserCommand command, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("DeleteUserCommandHandler.Handle called with {@Command}", command);
+
+        session.Delete(command.IdUser);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new DeleteUserResult(true);
+    }
+}
