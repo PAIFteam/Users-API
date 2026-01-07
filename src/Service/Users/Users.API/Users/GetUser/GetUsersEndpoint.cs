2f565809a31f0ab64cc04f894f53a38d3c3d4bd4
Caio Monteiro <caiomonteiropro@gmail.com>
2026-01-07 15:06:42 -0300

    finalizando o getUsers

--- src/Service/Users/Users.API/Users/GetUser/GetUsersEndpoint.cs
+++ src/Service/Users/Users.API/Users/GetUser/GetUsersEndpoint.cs
@@ -1,5 +1,24 @@
-﻿namespace Users.API.Users.GetUser; 
+﻿
+namespace Users.API.Users.GetUser; 
 
-public class GetUsersEndpoint
+//public record GetUsersRequest(); 
+public record GetUsersResponse(IEnumerable<UserModel> Users);
+public class GetUsersEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/users", async (ISender sender, CancellationToken cancellationToken) =>
+        {
+            var result = await sender.Send(new GetUsersQuery(), cancellationToken);
+
+            var response = result.Adapt<GetUsersResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetUsers")
+        .Produces<GetUsersResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Users")
+        .WithDescription("Get Users");
+    }
 }
--- src/Service/Users/Users.API/Users/GetUser/GetUsersHandler.cs
+++ src/Service/Users/Users.API/Users/GetUser/GetUsersHandler.cs
@@ -1,5 +1,17 @@
 ﻿namespace Users.API.Users.GetUser; 
 
-public class GetUsersHandler
+public record GetUsersQuery() : IQuery<GetUsersResult>;
+public record GetUsersResult(IEnumerable<UserModel> Users);
+internal class GetUsersQueryHandler(IDocumentSession session, ILogger<GetUsersQueryHandler> logger) : 
+    IQueryHandler<GetUsersQuery, GetUsersResult>
 {
+    public async Task<GetUsersResult> Handle(GetUsersQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetUsersQueryHandler.Handle called with {@Query}", query);
+
+        var users = await session.Query<UserModel>().ToListAsync(cancellationToken);
+
+        return new GetUsersResult(users);
+    }
 }
+ 
