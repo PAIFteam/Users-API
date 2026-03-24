56d6343d01ae5fa2eb0dbdb9a2f08643e7cb8d0d
fabriciolago <fabricio.r.lago@gmail.com>
2026-03-24 18:33:37 -0300

    Update Program.cs

--- src/Service/Users/Users.API/Program.cs
+++ src/Service/Users/Users.API/Program.cs
@@ -65,12 +65,33 @@ var app = builder.Build();
 
 if (!app.Environment.IsProduction())
 {
-    app.UseSwagger();
+    app.UseSwagger(c =>
+{
+    c.PreSerializeFilters.Add((swagger, httpReq) =>
+    {
+        var scheme = httpReq.Headers["X-Forwarded-Proto"].FirstOrDefault() ?? httpReq.Scheme;
+        var host = httpReq.Headers["X-Forwarded-Host"].FirstOrDefault() ?? httpReq.Host.Value;
+
+        var prefix = httpReq.Headers["X-Forwarded-Prefix"].FirstOrDefault();
+
+        if (string.IsNullOrWhiteSpace(prefix))
+        {
+            prefix = "/users";
+        }
+
+        swagger.Servers = new List<Microsoft.OpenApi.Models.OpenApiServer>
+        {
+            new() { Url = $"{scheme}://{host}{prefix}" }
+        };
+        });
+    });
     app.UseSwaggerUI(c =>
     {
         c.DisplayRequestDuration();
         c.EnablePersistAuthorization();
         c.DefaultModelsExpandDepth(2);
+
+        c.SwaggerEndpoint("./v1/swagger.json", "API v1");
     });
 }
 
